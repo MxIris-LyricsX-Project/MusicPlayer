d3f801cfc6ef92ce47087ebdf5d5f2bda84f8a97
Mx-Iris <mxiris@126.com>
2026-05-02 22:02:28 +0800

    chore: bump mediaremote-adapter to 0.1.2
    Pulls in the runPerlCommand --id propagation fix so that
    SystemMedia.allowsApplicationBundleIdentifiers actually filters
    update_player_state results, not just the listener loop.

--- Package.swift
+++ Package.swift
@@ -55,7 +55,7 @@ let package = Package(
             ),
             remote: .package(
                 url: "https://github.com/MxIris-LyricsX-Project/mediaremote-adapter",
-                from: "0.1.1"
+                from: "0.1.2"
             )
         ),
         
